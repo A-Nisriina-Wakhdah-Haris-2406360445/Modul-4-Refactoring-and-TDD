488b6f3289a908d2d85af996acf05329570a3b36
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-03-10 13:28:03 +0700

    [RED] Add tests for BankTransferPaymentStrategy

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/BankTFPaymentStrategyTest.java
@@ -0,0 +1,47 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTFPaymentStrategyTest {
+
+  @Test
+  void testProcessPaymentSuccess() {
+    BankTransferPaymentStrategy strategy = new BankTransferPaymentStrategy();
+
+    Map<String, String> paymentData = new HashMap<>();
+    paymentData.put("bankName", "BCA");
+    paymentData.put("referenceCode", "TRX123456");
+
+    String result = strategy.processPayment(paymentData);
+    assertEquals(PaymentStatus.SUCCESS.getValue(), result);
+  }
+
+  @Test
+  void testProcessPaymentBankNameNull() {
+    BankTransferPaymentStrategy strategy = new BankTransferPaymentStrategy();
+
+    Map<String, String> paymentData = new HashMap<>();
+    paymentData.put("referenceCode", "TRX123");
+
+    String result = strategy.processPayment(paymentData);
+    assertEquals(PaymentStatus.SUCCESS.getValue(), result);
+  }
+
+  @Test
+  void testProcessPaymentEmptyRefCode() {
+    BankTransferPaymentStrategy strategy = new BankTransferPaymentStrategy();
+
+    Map<String, String> paymentData = new HashMap<>();
+    paymentData.put("bankName", "BCA");
+    paymentData.put("referenceCode", "");
+
+    String result = strategy.processPayment(paymentData);
+    assertEquals(PaymentStatus.REJECTED.getValue(), result);
+  }
+}
